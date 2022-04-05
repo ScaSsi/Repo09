3653fdef926e33ffe45ef0ba8e178e8dd035d95a
Avujcic1 <avujcic1@etf.unsa.ba>
2022-04-05 21:56:43 +0200

    Function - Do while

---
+++ ex19/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+function myDoWhile() {
+    var myNumbers = "";
+    var i = 0 ;
+    do {
+        myNumbers += i;
+        if(i !== 9) myNumbers += ", ";
+        i++;
+    }while(i < 10);
+
+    return myNumbers;
+}
+
+myDoWhile();
+console.log(myDoWhile());
+// only change code above this line
+module.exports = myDoWhile;
